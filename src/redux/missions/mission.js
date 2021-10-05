03a1efb692288ab454bf887dec1cb1b5eb62db43
Daniel Samuel <dansam5000@gmail.com>
2021-10-05 18:01:54 +0100

    feat:implement fetch mission data

---
+++ src/redux/missions/mission.js
@@ -0,0 +1,23 @@
+const baseURL = 'https://api.spacexdata.com/v3/missions';
+const initialState = [];
+
+const ADD_MISSION = 'missions/ADD_MISSION';
+const JOIN_OR_LEAVE_MISSION = 'missions/JOIN_OR_LEAVE_MISSION';
+
+export const fetchMissions = async (dispatch) => {
+  const fetched = await fetch(`${baseURL}`);
+  const returnList = await fetched.json();
+  const missions = [];
+  returnList.map((mission) =>
+    missions.push({
+      mission_id: mission.mission_id,
+      mission_name: mission.mission_name,
+      description: mission.description,
+      wikipedia: mission.wikipedia,
+    })
+  );
+  dispatch({
+    type: ADD_MISSION,
+    payload: missions,
+  });
+};
