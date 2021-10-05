3772b9e71d34e801e374cbba2970e98b983a29e8
Daniel Samuel <dansam5000@gmail.com>
2021-10-05 17:33:54 +0100

    chore: updating build task with thunk dependencies

--- src/App.js
+++ src/App.js
@@ -1,30 +1,26 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/configureStore';
 import Navbar from './components/Navbar';
 import Rockets from './pages/Rockets';
 import Missions from './pages/Missions';
 import MyProfile from './pages/MyProfile';
 
 const App = () => (
-  <>
+  <Provider store={store}>
     <Router>
       <Navbar />
       <Switch>
         <div className="App">
           <Route exact path="/" component={Rockets} />
-          <Route
-            path="/missions"
-            component={Missions}
-          />
+          <Route path="/missions" component={Missions} />
           |
-          <Route
-            path="/my-profile"
-            component={MyProfile}
-          />
+          <Route path="/my-profile" component={MyProfile} />
         </div>
       </Switch>
     </Router>
-  </>
+  </Provider>
 );
 
 export default App;
