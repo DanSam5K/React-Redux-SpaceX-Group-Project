de2b2e725121aeb93407b09c4e01757566d7e5f4
rotimiazeez <harzeyzhrotimi@gmail.com>
2021-10-06 19:12:30 +0100

    feat: Add rocket info to profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,4 +1,5 @@
-import React from 'react';
+/* eslint-disable linebreak-style */
+// import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import {
   Card,
@@ -10,8 +11,29 @@ import {
   Button,
 } from 'react-bootstrap';
 import { missions, toggleMission } from '../redux/missions/mission';
+import { rockets } from '../redux/rockets/rocket';
+import { removeReservation } from '../redux/api/api';
+
+// const Group = (data) => {
+//   const items = data.data;
+//   return items.map((g) => (
+//     <div key={g.title} className="reservation">
+//       <h5 className="reservation-item-name">{g}</h5>
+//     </div>
+//   ));
+// };
 
 const Profile = () => {
+  // const [rocketsDisplay, setRocketsDisplay] = useState(null);
+  // const reservations = useSelector((state) => state.reservationsReducer.reservations);
+
+  // useEffect(() => {
+  //   const { rockets } = reservations;
+  //   if (rockets.length > 0) {
+  //     setRocketsDisplay(rockets);
+  //   }
+  // });
+
   const allMissions = useSelector(missions);
   const dispatch = useDispatch();
 
@@ -42,6 +64,29 @@ const Profile = () => {
         </Button>
       </ListGroupItem>
     ));
+  const reserveRockets = rockets.filter((rocket) => rocket.reserved).length === 0
+    ? <ListGroupItem> No rockets reserved </ListGroupItem>
+    : rockets = rockets.filter((rocket) => rocket.reserved).map((rocket) => (
+      <ListGroupItem
+        key={rocket.rocket_id}
+        className="d-flex align-items-center justify-content-between"
+      >
+        <span>
+          {rocket.rocket_name}
+          <br />
+          <a href={rocket.wikipedia} target="blank">
+            Read more
+          </a>
+        </span>
+        <Button
+          variant="outline-danger"
+          id={rocket.rocket}
+          onClick={() => dispatch(removeReservation(rocket.rocket_id))}
+        >
+          Cancel Reservation
+        </Button>
+      </ListGroupItem>
+    ));
   return (
     <Container fluid className="border-top w-100 pt-2">
       <Row>
@@ -60,7 +105,9 @@ const Profile = () => {
         <Col xs={12} md={6}>
           <h2>My Rockets</h2>
           <Card>
-            <ListGroup>reservedRockets</ListGroup>
+            <ListGroup>
+              { reserveRockets }
+            </ListGroup>
           </Card>
         </Col>
       </Row>
