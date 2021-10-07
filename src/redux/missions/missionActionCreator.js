04d2c0f21d55f2aba6e6f625353669b949fcde04
Daniel Samuel <dansam5000@gmail.com>
2021-10-07 23:03:37 +0100

    mod: Modify mission actions types to use ducks pattern

--- src/redux/missions/missionActionCreator.js
+++ src/redux/missions/missionActionCreator.js
@@ -2,7 +2,7 @@ import * as actions from './missionActions';
 import { missionBaseURL } from '../apis';
 
 export const toggleMission = (mission) => ({
-  type: actions.JOIN_OR_LEAVE_MISSION,
+  type: actions.JOIN_MISSION,
   payload: mission,
 });
 
@@ -10,12 +10,14 @@ export const fetchMissions = async (dispatch) => {
   const fetchMissions = await fetch(`${missionBaseURL}`);
   const returnList = await fetchMissions.json();
   const missions = [];
-  returnList.map((mission) => missions.push({
-    mission_id: mission.mission_id,
-    mission_name: mission.mission_name,
-    description: mission.description,
-    wikipedia: mission.wikipedia,
-  }));
+  returnList.map((mission) =>
+    missions.push({
+      mission_id: mission.mission_id,
+      mission_name: mission.mission_name,
+      description: mission.description,
+      wikipedia: mission.wikipedia,
+    })
+  );
   dispatch({
     type: actions.ADD_MISSION,
     payload: missions,
