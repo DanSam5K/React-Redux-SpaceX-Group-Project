b9f3f3c9cda382b54d47560448d157c15a772f66
Daniel Samuel <dansam5000@gmail.com>
2021-10-07 08:28:45 +0100

    mod: mmodify rockets feature into component

---
+++ src/components/Rockets.js
@@ -0,0 +1,26 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchRockets } from '../redux/rockets/rocket';
+import Rocket from './Rocket';
+
+const Rockets = () => {
+  const rockets = useSelector((state) => state.rocketsReducer);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (!rockets.length > 0) {
+      dispatch(fetchRockets());
+    }
+  }, []);
+
+  return (
+    <div>
+      <ul>
+        {rockets.map((rocket) => (
+          <Rocket key={rocket.id} rocket={rocket} />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Rockets;
